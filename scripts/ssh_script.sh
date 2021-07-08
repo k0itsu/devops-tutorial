75ad398830b2b01e49189e4e54146ad7b049c831
Edward Delin <eldelin@gmail.com>
2021-07-08 01:00:31 -0400

    try fix for copying the private and public keys to hosts

--- scripts/ssh_script.sh
+++ scripts/ssh_script.sh
@@ -2,10 +2,11 @@
 #!/usr/bin/env bash
 
 mkdir -pm 700 /home/vagrant/.ssh
+
+
+curl --insecure https://raw.githubusercontent.com/k0itsu/devops-tutorial/main/scripts/ssh_files/public_key >> /home/vagrant/.ssh/authorized_keys
+curl --insecure https://raw.githubusercontent.com/k0itsu/devops-tutorial/main/scripts/ssh_files/private_key > /home/vagrant/.ssh/id_rsa
+
 chown -R vagrant:vagrant /home/vagrant/.ssh
-
-curl --insecure https://raw.githubusercontent.com/k0itsu/devops-tutorial/main/scripts/ssh_files/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-curl --insecure https://raw.githubusercontent.com/k0itsu/devops-tutorial/main/scripts/ssh_files/id_rsa > /home/vagrant/.ssh/id_rsa
-
 chmod 644 /home/vagrant/.ssh/authorized_keys
 chmod 600 /home/vagrant/.ssh/id_rsa
